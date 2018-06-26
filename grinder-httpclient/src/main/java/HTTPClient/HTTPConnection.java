ba7ae0679782ee03bdd6646d65a0bf2fbf9f2752
ome13 <omerlin13@gmail.com>
2018-06-26 16:13:20 +0200

    when Host header is already defined, don't append port

--- grinder-httpclient/src/main/java/HTTPClient/HTTPConnection.java
+++ grinder-httpclient/src/main/java/HTTPClient/HTTPConnection.java
@@ -3799,11 +3799,11 @@ public class HTTPConnection implements GlobalConstants, HTTPClientModuleConstant
 	    dataout.writeBytes(req.getMethod() + " " + file + " " +
 			       RequestProtocolVersion + "\r\n");
 
-	String h_hdr = (ho_idx >= 0) ? hdrs[ho_idx].getValue().trim() : Host;
-	if (Port != URI.defaultPort(getProtocol()))
-	    dataout.writeBytes("Host: " + h_hdr + ":" + Port + "\r\n");
-	else    // Netscape-Enterprise has some bugs...
-	    dataout.writeBytes("Host: " + h_hdr + "\r\n");
+//	String h_hdr = (ho_idx >= 0) ? hdrs[ho_idx].getValue().trim() : Host;
+//	if (Port != URI.defaultPort(getProtocol()))
+//	    dataout.writeBytes("Host: " + h_hdr + ":" + Port + "\r\n");
+//	else    // Netscape-Enterprise has some bugs...
+	dataout.writeBytes("Host: " + h_hdr + "\r\n");
 
 
 	/*
